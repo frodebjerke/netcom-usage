2a2c1dea8829fa37690cce11e5655b730ec217ee
Frode Bjerke <frode.bjerke@gmail.com>
2015-07-16 21:27:20 +0200

    refactor and log result to file

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    targetSite: process.env.TARGET_SITE || 'netcom.no/mbb-refill'
+    targetSite: process.env.TARGET_SITE || 'netcom.no/mbb-refill',
+    usageLog: process.env.LOG_FILE || 'measurements.log'
 }
--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-var ScrapeSite = require('./lib/scrape-site');
-var config = require('./config');
+const co = require('co');
+const scraper = require('./lib/scraper');
+const log = require('./lib/logging');
+const config = require('./config');
 
-var scraper = new ScrapeSite(config);
-
-scraper.scrape()
-    .then((res) => {
-        console.log('Data left:', res);
-        console.log('Success. Quitting ...');
-    })
-    .catch((err) => {
-        console.log('Catastrophic failure:', err)
-        console.log('Script failed to execute. Quitting ...');
-    });
+co(function* () {
+    console.log('STEP 1: Fetch and extract');
+    const dataleft = yield scraper.scrape(config.targetSite);
+    return {dataleft: dataleft};
+})
+.then(function (res) {
+    console.log('STEP 2: Log to file');
+    const usageLogger = log.getUsageLogger(config.usageLog);
+    usageLogger(res.dataleft + 'MB left', res);
+    return res;
+})
+.then(function (res) {
+    console.log('Success:', res);
+})
+.catch(function (error) {
+    console.log('Failure:', error);
+});
